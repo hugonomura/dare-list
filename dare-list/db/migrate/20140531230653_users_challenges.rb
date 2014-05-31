cdef832e34567f06ea7f71c0938058a4e0d1aaff
André Mitsuoka <andrermitsuoka@gmail.com>
2014-05-31 20:21:33 -0300

    fix migration has_and_belongs_to_many

--- dare-list/db/migrate/20140531230653_users_challenges.rb
+++ dare-list/db/migrate/20140531230653_users_challenges.rb
@@ -1,8 +1,10 @@
 class UsersChallenges < ActiveRecord::Migration
+
   def change
-    create_table users_challenges do |t|
+    create_table :users_challenges,id: false do |t|
       t.belongs_to :user
       t.belongs_to :challenge
     end
   end
+
 end
