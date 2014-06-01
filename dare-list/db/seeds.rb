49325486d4e6fe4df1c8a7ceda6aa5e41088393d
André Mitsuoka <andrermitsuoka@gmail.com>
2014-06-01 06:02:19 -0300

    seed challenges

--- dare-list/db/seeds.rb
+++ dare-list/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Challenge.create(name: 'feijoada', type: false, description: "blah blah", date: Date.tomorrow)
+Challenge.create(name: 'caipirinha', type: false, description: "caipi caipi blah", date: Date.tomorrow)
+Challenge.create(name: 'por do sol', type: true, description: "sol sol blah", date: Date.tomorrow)
+Challenge.create(name: 'pastel de flango', type: true, description: "uhhhh flango", date: Date.tomorrow)
+Challenge.create(name: 'surfar no ocenao', type: true, description: "surf surf", date: Date.tomorrow)
+
+
+
+
