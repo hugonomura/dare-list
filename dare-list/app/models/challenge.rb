419e9ee81027bce08e480c570360e2dfc3681ed4
André Mitsuoka <andrermitsuoka@gmail.com>
2014-05-31 23:18:52 -0300

    read csv

--- dare-list/app/models/challenge.rb
+++ dare-list/app/models/challenge.rb
@@ -1,8 +1,33 @@
+require 'date'
+require 'csv' 
+
 class Challenge < ActiveRecord::Base
 
-  #attr_accessible :name,:type,:description,:date
 
   has_many :users , through: :users_challenges
   has_many :users_challenges
+
+
+  def self.read_csv(csv_file)
+    CSV.foreach(csv_file, {:headers => false,:col_sep => ";"}) do |row|
+      name = row[0]
+      type = row[1]
+
+      unless type == "true"
+        type = false
+      else
+        type = true
+      end
+      
+      description = row[2]
+      date = row[3]
+      date = Date.parse( date.gsub(/, */, '-') )
+
+      unless name.nil? || type.nil? || description.nil? || date.nil?
+        Challenge.create(name: name,type: type,description: description,date: date)
+      end
+
+      end
+  end
 
 end
--- dare-list/db/migrate/20140531230653_users_challenges.rb
+++ dare-list/db/migrate/20140531230653_users_challenges.rb
@@ -5,6 +5,7 @@ class UsersChallenges < ActiveRecord::Migration
       t.belongs_to :user
       t.belongs_to :challenge
       t.boolean :concluded
+      t.date :date
     end
 
   end
