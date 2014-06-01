2bd98f83c6b9cf720586190ac34e139388838023
André Mitsuoka <andrermitsuoka@gmail.com>
2014-06-01 08:07:52 -0300

    seeds + migration

--- dare-list/db/migrate/20140531225047_create_challenges.rb
+++ dare-list/db/migrate/20140531225047_create_challenges.rb
@@ -2,7 +2,7 @@ class CreateChallenges < ActiveRecord::Migration
   def change
     create_table :challenges do |t|
       t.string :name
-      t.boolean :type
+      t.boolean :stadium
       t.text :description
       t.date :date
 
