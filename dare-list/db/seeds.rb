c73c2708766c5e5840e53bc80d4dd311cb825b63
André Mitsuoka <andrermitsuoka@gmail.com>
2014-06-01 06:25:10 -0300

    challenges seed fix

--- dare-list/db/seeds.rb
+++ dare-list/db/seeds.rb
@@ -7,11 +7,11 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-Challenge.create(name: 'feijoada', type: false, description: "blah blah", date: Date.tomorrow)
+Challenge.create(name: 'feijoada', type: false  , description: "blah blah", date: Date.tomorrow)
 Challenge.create(name: 'caipirinha', type: false, description: "caipi caipi blah", date: Date.tomorrow)
-Challenge.create(name: 'por do sol', type: true, description: "sol sol blah", date: Date.tomorrow)
-Challenge.create(name: 'pastel de flango', type: true, description: "uhhhh flango", date: Date.tomorrow)
-Challenge.create(name: 'surfar no ocenao', type: true, description: "surf surf", date: Date.tomorrow)
+Challenge.create(name: 'por do sol', type: false , description: "sol sol blah", date: Date.tomorrow)
+Challenge.create(name: 'pastel de flango', type: false, description: "uhhhh flango", date: Date.tomorrow)
+Challenge.create(name: 'surfar no ocenao', type: false, description: "surf surf", date: Date.tomorrow)
 
 
 
