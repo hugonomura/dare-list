1a08e83079841ecc2b195b37925362a2ba0a68cd
Hugo Nomura <hugonomura@gmail.com>
2014-06-01 04:31:55 -0300

    criando uma todo list

--- dare-list/app/helpers/application_helper.rb
+++ dare-list/app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def create_todo_list(user)
+    note = Note.new
+    for challenge in user.challenges
+      note.challenges << challenge
+    end
+    note.save
+  end
 end
--- dare-list/app/models/note.rb
+++ dare-list/app/models/note.rb
@@ -1,18 +1,30 @@
 require "evernote_oauth"
 
 class Note
-  attr_accessor :body
+  attr_accessor :content_body, :challenges
 
   def initialize
     @title = "Hackathon Taça"
-    @body = ""
+
+    @start_body = '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd"><en-note>'
+    @content_body = ''
+    @end_body = '</en-note>'
+    @body = @start_body + @content_body + @end_body
+
+    @challenges = []
+
     @parent_notebook = Notebook.new
   end
 
   def save
     note = Evernote::EDAM::Type::Note.new
     note.title = @title
-    note.content = @body
+    @body = @start_body + @content_body + @end_body
+
+    for challenge in challenges
+      @content_body += '<en-todo checked="false"/><b>' + challenge.name + '</b> - ' + challenge.description + '<br />' 
+    end
+
     note.notebookGuid = @parent_notebook.guid
 
     note_ok = DareList::Application.note_store.createNote(note)
